899ca8b50852d4f743a13137ef1bab8774580f05
likelion-hoon <monki0120@naver.com>
2016-11-24 21:01:20 +0900

    각각의 프레그먼트 형식 불러오기 코딩중  (월-스태거그리드, 주-listview, 일-gridview)

--- app/src/main/java/com/example/ljh/termproject1/WeekFragment.java
+++ app/src/main/java/com/example/ljh/termproject1/WeekFragment.java
@@ -7,10 +7,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class WeekFragment extends Fragment {
+
+    private ListView lvweek;
+    private String[] weeks;
+
     public WeekFragment() {
         // Required empty public constructor
     }
@@ -18,7 +23,21 @@ public class WeekFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_week, container, false);
+
+        weeks = getActivity().getResources().getStringArray(R.array.weeks);
+
+        // listview의 아이디를 가져온다.
+        lvweek = (ListView) view.findViewById(R.id.listview);
+
+        lvweek.setAdapter(new ArrayAdapter<String>(getActivity().getApplicationContext(),
+                android.R.layout.simple_list_item_1, weeks));
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_week, container, false);
     }
+
+
+
 }
